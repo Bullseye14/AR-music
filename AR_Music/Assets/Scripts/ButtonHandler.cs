a5471be180f916cdb7b3ab3b8e2ab21235b8a5db
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2021-05-14 14:18:36 +0200

    new button WIP

---
+++ AR_Music/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public AudioClip[] aClips;
+    public AudioSource myAudioSource;
+
+    public void ChangeInstrument_()
+    {
+        PlayNote(0);
+    }
+
+    private void PlayNote(int color)
+    {
+        myAudioSource.clip = aClips[color];
+        myAudioSource.Play();
+    }
+}
