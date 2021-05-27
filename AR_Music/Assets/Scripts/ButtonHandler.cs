797d0e9e8c37d05e6525ebaedc112df80a3b1eb8
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2021-05-27 08:56:25 +0200

    Changed buttons display, last commit

--- AR_Music/Assets/Scripts/ButtonHandler.cs
+++ AR_Music/Assets/Scripts/ButtonHandler.cs
@@ -5,31 +5,40 @@ using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
-    public Sprite guitarSprite;
-    public Sprite trumpetSprite;
-    public Sprite pianoSprite;
+    public Sprite normalSprite;
+    public Sprite selectedSprite;
+
+    public Sprite otherSprite1, otherSprite2;
 
     public Button button;
+
+    // Passem a cada botó els altres dos botons amb els seus sprites normals
+    public Button otherButton1, otherButton2;
 
     public SoundTest soundTest;
 
     public void ChangeInstrument_()
     {
-        switch(soundTest.instrument)
+        // Cada vegada que cliquem un botó, aquest canviarà a selected, i els altres dos a no selected
+        button.image.sprite = selectedSprite;
+        otherButton1.image.sprite = otherSprite1;
+        otherButton2.image.sprite = otherSprite2;
+
+        switch (button.name)
         {
-            case 1:
-                soundTest.instrument = 2;
-                button.image.sprite = guitarSprite;
-                break;
-
-            case 2:
-                soundTest.instrument = 3;
-                button.image.sprite = trumpetSprite;
-                break;
-
-            case 3:
+            case "PianoButton":
                 soundTest.instrument = 1;
-                button.image.sprite = pianoSprite;
+                break;
+
+            case "GuitarButton":
+                soundTest.instrument = 2;
+                break;
+
+            case "TrumpetButton":
+                soundTest.instrument = 3;
+                break;
+
+            default:
                 break;
         }
     }
